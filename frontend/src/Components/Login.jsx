16c2a978ac2458d2ff6f5a6cfafa6ab2155c6ae3
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-01-21 23:24:34 +0500

    add pull request and resolve conflicts

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,58 +1,6 @@
 import React from "react";
 import LoginImage from "../images/log1.jpg";
-<<<<<<< HEAD
 
-
-function Login(props) {
-=======
-
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
-
-const Login = () => {
->>>>>>> dc4ef7676895adf7114936c078d777bea0a6bc56
-  return (
-    <div className="flex flex-col md:flex-row h-screen">
-      {/* Left side */}
-      <div className="flex w-1/2 border-black-100 border-5">
-        <div className="flex flex-col gap-8 p-8 bg-bg-color text-white w-full">
-       
-          <div className="flex flex-col gap-5">
-            <h2 className="text-5xl font-bold">Login</h2>
-            <p className="text-2xl">Enter your Credentials to Login</p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="text-black">
-              <p className="text-white pl-1">Email</p>
-              <input
-                className="p-2 rounded-lg w-full"
-                placeholder="Enter Email"
-              />
-            </div>
-            <div className="text-black mt-4">
-              <p className="text-white pl-1">Password</p>
-              <input
-                className="p-2 rounded-lg w-full"
-                placeholder="Enter Password"
-              />
-            </div>
-          </div>
-          <button className="bg-blue-500 text-white p-2 rounded-lg w-full">
-            Login
-          </button>
-        </div>
-      </div>
-
-      {/* Right side */}
-      <div className="w-1/2 border-gray-100 border-5">
-        <img src={LoginImage} alt="login" className="w-full h-full object-cover" />
-      </div>
-    </div>
-  );
-<<<<<<< HEAD
-}
-=======
-};
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 
@@ -108,7 +56,5 @@ function Login(props) {
         </div>
     );
 }
-
->>>>>>> dc4ef7676895adf7114936c078d777bea0a6bc56
 
 export default Login;
