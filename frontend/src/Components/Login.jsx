00897ec2c917a00b3c9e97b4ea084925bd863229
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-01-26 01:19:08 +0500

    API

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,27 +1,69 @@
 import React, { useState } from "react";
+import axios from "axios";
 import LoginImage from "../images/log1.jpg";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
 import InputField from "./InputField";
+import validator from "validator";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+  const [errors, setErrors] = useState({});
+  const [apiError, setApiError] = useState("");
+  const [user, setUser] = useState("");
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: "" });
+    setApiError("");
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    if (!validator.isEmail(formData.email)) {
+      newErrors.email = "Invalid email format";
+      isValid = false;
+    }
+    if (validator.isEmpty(formData.email)) {
+      newErrors.email = "Email is required";
+      isValid = false;
+    }
+
+    if (validator.isEmpty(formData.password)) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
   };
 
-  const handleSubmit = (event) => {
-    // Handle form submission, e.g., make an API call
-    event.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
-    // Add your logic for form submission here
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/Login",
+        formData
+      );
+
+      if (response.data) {
+        setUser(response.data);
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+      setApiError(error.response.data.error);
+    }
   };
 
   return (
@@ -29,47 +71,72 @@ const Login = () => {
       {/* Left Section */}
       <div className="w-full md:w-1/2 p-4 h-full">
         <ArrowBackIcon className="cursor-pointer absolute top-8 left-8 text-black" />
-        <img src={LoginImage} alt="login" className="w-full h-full object-cover" />
+        <img
+          src={LoginImage}
+          alt="login"
+          className="w-full h-full object-cover"
+        />
       </div>
 
       {/* Right Section */}
       <div className="bg-bg-color w-full md:w-1/2 p-4 flex items-center justify-center h-full">
         <div className="bg-bg-color p-8 text-center max-w-md mx-auto">
           <h2 className="text-5xl font-bold mb-4 text-white">Login</h2>
-          <p className="text-lg text-white mb-6">Enter your credentials to login</p>
+          <p className="text-lg text-white mb-6">
+            Enter your credentials to login
+          </p>
 
-          <form className="grid grid-cols-1 gap-6 mt-20" onSubmit={handleSubmit}>
+          <form className="grid grid-cols-1 gap-6 mt-20" onSubmit={handleLogin}>
             {/* Email */}
-            <InputField
-              label="Email"
-              type="text"
-              id="email"
-              name="email"
-              autoComplete="off"
-              value={email}
-              onChange={handleEmailChange}
-              className={email ? "has-value" : ""}
-            />
-
+            <div>
+              <InputField
+                label="Email"
+                type="text"
+                id="email"
+                name="email"
+                value={formData.email}
+                autoComplete="off"
+                onChange={handleInputChange}
+              />
+              {errors.email && (
+                <p className="text-red-800 font-bold text-xs text-left">
+                  {errors.email}
+                </p>
+              )}
+            </div>
             {/* Password */}
-            <InputField
-              label="Password"
-              type="password"
-              id="password"
-              name="password"
-              autoComplete="off"
-              value={password}
-              onChange={handlePasswordChange}
-            />
+            <div>
+              <InputField
+                label="Password"
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                autoComplete="off"
+                onChange={handleInputChange}
+              />
+              {errors.password && (
+                <p className="text-red-800 font-bold text-xs text-left">
+                  {errors.password}
+                </p>
+              )}
+            </div>
+            {apiError && (
+              <p className="text-red-800 font-bold text-xs text-left w-full">
+                {apiError}
+              </p>
+            )}
 
             {/* Submit Button */}
             <div className="mb-4">
-              <button type="submit" className="flex justify-center bg-sec-color text-white p-1 rounded cursor-pointer w-full mt-10">
+              <button className="flex justify-center bg-sec-color text-white p-1 rounded cursor-pointer w-full mt-4">
                 <LoginOutlinedIcon className="mr-2" />
                 <p className="text-lg font-bold">Login</p>
               </button>
             </div>
           </form>
+
+          {/* API Error */}
         </div>
       </div>
     </div>
