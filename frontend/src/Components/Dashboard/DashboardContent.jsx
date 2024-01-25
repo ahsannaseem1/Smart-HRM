6752e6dd84fcfc4d33680f27fc9a4d7a792bb89f
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-01-25 18:32:12 +0500

    Dashboard

--- frontend/src/Components/Dashboard/DashboardContent.jsx
+++ frontend/src/Components/Dashboard/DashboardContent.jsx
@@ -1,5 +1,6 @@
 // src/components/Dashboard/DashboardContent.jsx
 import React, { useState } from "react";
+import DashboardStat from "./DashboardCard";
 
 function DashboardContent() {
   const [toDoList, setToDoList] = useState([
@@ -93,14 +94,5 @@ function DashboardContent() {
   );
 }
 
-// Dashboard Stat Component
-function DashboardStat({ label, value }) {
-  return (
-    <div className="p-4 bg-white rounded-md shadow-md">
-      <p className="text-lg font-semibold mb-2">{value}</p>
-      <p className="text-gray-500">{label}</p>
-    </div>
-  );
-}
 
 export default DashboardContent;
