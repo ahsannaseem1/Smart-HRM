5829d578da9055096b18056edb0da86c19e2d63f
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-02-19 14:34:02 +0500

    filter keyword

--- frontend/src/Components/Dashboard/Recruitment/Applicants.jsx
+++ frontend/src/Components/Dashboard/Recruitment/Applicants.jsx
@@ -6,6 +6,7 @@ import DashboardOverview from "../DashboardOverview";
 import { pdfjs, Document, Page } from "react-pdf";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import InputField from "../../Styles/InputField";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function Applicants() {
   const { organizationId, jobId } = useParams();
@@ -14,6 +15,8 @@ function Applicants() {
   const [pdfMetadata, setPdfMetadata] = useState(null);
   const [pdfTextContent, setPdfTextContent] = useState("");
   const [filterKeywords, setFilterKeywords] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [apiError, setApiError] = useState("");
 
   useEffect(() => {
     const getApplicants = async () => {
@@ -21,8 +24,12 @@ function Applicants() {
         const response = await axios.get(
           `http://localhost:5000/GetApplicants/?organizationId=${organizationId}&jobId=${jobId}`
         );
+
+        setLoading(false);
         setApplicants(response.data);
       } catch (error) {
+        setApiError(error.response.data);
+        setLoading(false);
         console.error("Error fetching applicants:", error);
       }
     };
@@ -63,16 +70,22 @@ function Applicants() {
 
   const handleFilter = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
         `http://localhost:5000/GetTopApplicants/?organizationId=${organizationId}&jobId=${jobId}&job_description=${filterKeywords}`
       );
+      setLoading(false);
       const filteredApplicants = response.data.ranking;
-      
+
       // Sort applicants based on ranking
-      const sortedApplicants = filteredApplicants.sort((a, b) => a.ranking - b.ranking);
-      
+      const sortedApplicants = filteredApplicants.sort(
+        (a, b) => a.ranking - b.ranking
+      );
+
       setApplicants(sortedApplicants);
     } catch (error) {
+      setLoading(false);
+      setApiError(error.response.data);
       console.error("Error filtering applicants:", error);
     }
   };
@@ -82,7 +95,16 @@ function Applicants() {
   }, []);
 
   return (
-    <div className="flex gap-4">
+    <div
+      className={`flex gap-4 ${
+        loading ? "pointer-events-none opacity-70" : ""
+      }`}
+    >
+      {loading && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <CircularProgress color="primary" />
+        </div>
+      )}
       <div>
         <Sidebar></Sidebar>
       </div>
@@ -112,30 +134,33 @@ function Applicants() {
             <FilterAltIcon className="mr-1"></FilterAltIcon>Filter
           </button>
         </div>
-        <div className="flex flex-col flex-wrap  pt-6 justify-between p-3 w-11/12 m-auto bg-sec-color rounded-lg text-white">
-          {applicants.map((applicant) => (
-            <div key={applicant.id} className=" mb-4 w-full text-sec-color">
-              <div className="flex justify-between bg-white p-3 rounded-lg shadow-md">
-                <div>
-                  <p className="text-sm font-bold">{applicant.name}</p>
-                  <p className="text-sm">{applicant.email}</p>
-                  <p className="text-sm">{applicant.phoneNumber}</p>
-                </div>
-                <div className="flex justify-end mt-2">
-                  <button className="p-2 text-sm mr-2 bg-sec-color text-white rounded-lg active:text-sec-color active:bg-white">
-                    View CV
-                  </button>
-                  <button
-                    className="p-2 text-sm bg-sec-color text-white rounded-lg active:text-sec-color active:bg-white"
-                    onClick={() => handlePdfClick(applicant.cv)}
-                  >
-                    Download CV
-                  </button>
+        {!loading && (
+          <div className="flex flex-col flex-wrap  pt-6 justify-between p-3 w-11/12 m-auto bg-sec-color rounded-lg text-white">
+            {applicants.map((applicant) => (
+              <div key={applicant._id} className=" mb-4 w-full text-sec-color">
+                <div className="flex justify-between bg-white p-3 rounded-lg shadow-md">
+                  <div>
+                    <p className="text-sm font-bold">{applicant.name}</p>
+                    <p className="text-sm">{applicant.email}</p>
+                    <p className="text-sm">{applicant.phoneNumber}</p>
+                  </div>
+                  <div className="flex justify-end mt-2">
+                    <button className="p-2 text-sm mr-2 bg-sec-color text-white rounded-lg active:text-sec-color active:bg-white">
+                      View CV
+                    </button>
+                    <button
+                      className="p-2 text-sm bg-sec-color text-white rounded-lg active:text-sec-color active:bg-white"
+                      onClick={() => handlePdfClick(applicant.cv)}
+                    >
+                      Download CV
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+        {apiError && (<p className="text-red-500">{apiError}</p>)}
       </div>
     </div>
   );
